103ca614eb4a978309bd7d70451e8ad08e157f1f
gowsalya05 <gowsalya4565@gmail.com>
2026-09-19 11:27:54 +0530

    Implement basic input and output in basics.py
    Add input and output statements for basic operations

---
+++ Python/basics.py
@@ -0,0 +1,12 @@
+#########INPUT STATEMENT############
+a=int(input('Enter value: '))#single value datatype
+b=eval(input('Enter value: '))#input store in double quotes in eval()\
+b=(input('Enter the value: '))#input doesn't need to give in double quotes for string it automatically replace with string
+
+##########OUTPUT STATEMENT##########
+a=10
+b=20
+print(a,b)
+print(a,b,sep='_________')
+print(a,b,sep=',',end=('\ncompleted\n'))
+
